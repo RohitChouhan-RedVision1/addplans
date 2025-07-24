88d925a34f9e0348cd477c1cf5fdbbba8e5f3654
RohitChouhan-RedVision1 <Rohit.Chouhan@redvisionglobal.com>
2025-07-24 11:34:53 +0530

    changes done

--- src/app/(main)/contact-us/page.jsx
+++ src/app/(main)/contact-us/page.jsx
@@ -4,6 +4,7 @@ import { getSiteData } from "@/lib/functions";
 import { MapPin, Phone, Mail } from "lucide-react";
 import Link from "next/link";
 import RvBreadcrumbs from "@/components/landing/page-breadcrumbs/rvbreadcrumbs";
+import Image from "next/image";
 
 export default async function ContactUs() {
   const sitedata = await getSiteData();
@@ -19,17 +20,17 @@ export default async function ContactUs() {
         <div className="flex  flex-col  gap-4 overflow-hidden rounded-lg mb-8">
           {/* Call Us */}
           <div className="flex flex-col md:flex-row gap-4 w-full justify-center items-center text-center">
-            <div className="bg-[var(--rv-primary)] text-white p-8 rounded shadow-md w-full md:w-1/3 flex flex-col items-center justify-center min-h-[200px]">
+            <div className="bg-[var(--rv-primary)] text-white p-8 rounded shadow-md w-full md:w-1/3 flex flex-col items-center justify-center min-h-[220px]">
               <Phone className="mb-2" size={24} />
               <h3 className="font-medium mb-2">Call Us</h3>
               <p className=""><Link href={`tel:+91${sitedata.mobile}`} >+91 {sitedata.mobile}</Link></p>
             </div>
-            <div className="bg-[var(--rv-primary)] text-white p-8 rounded shadow-md w-full md:w-1/3 flex flex-col items-center justify-center min-h-[200px] ">
+            <div className="bg-[var(--rv-primary)] text-white p-8 rounded shadow-md w-full md:w-1/3 flex flex-col items-center justify-center min-h-[220px] ">
               <Mail className="mb-2" size={24} />
               <h3 className="font-medium mb-2">Mail Us</h3>
               <p className="break-all"><p><Link href={`mailto:${sitedata.email}`} >{sitedata.email}</Link></p></p>
             </div>
-            <div className="bg-[var(--rv-primary)] text-white p-8 rounded shadow-md w-full md:w-1/3 flex flex-col items-center justify-center min-h-[200px]">
+            <div className="bg-[var(--rv-primary)] text-white p-8 rounded shadow-md w-full md:w-1/3 flex flex-col items-center justify-center min-h-[220px]">
               <MapPin className="mb-1" size={24} />
               <h3 className="font-medium mb-1">Reach Us</h3>
               <p className="break-all">
@@ -55,7 +56,7 @@ export default async function ContactUs() {
         {/* Map and Contact Form */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
           {/* Map */}
-          <div className="rounded w-full h-[640px] relative border border-gray-200">
+          <div className="rounded w-full h-[500px] relative border border-gray-200">
             <iframe
               src={sitedata?.iframe}
               width="100%"
@@ -69,11 +70,18 @@ export default async function ContactUs() {
 
           {/* Contact Form */}
           <div className="w-full">
-            <div className="px-[75px] py-[56px] bg-[var(--rv-white)] animate fadeInRight wow" data-wow-duration="1500ms"
+            <div className=" bg-[var(--rv-white)] animate fadeInRight wow" data-wow-duration="1500ms"
               style={{
                 clipPath: "polygon(0 0, 100% 0, 100% 0, 100% 79%, 86% 100%, 0 100%, 0 100%, 0 0)"
               }}>
-              <ContactReusableForm sitedata={sitedata} />
+              <Image
+                                                               src="/images/contact/contactus.jpg"
+                                                               alt="Contact us"
+                                                               width={1000}
+                                                               height={500} // Adjust as needed for aspect ratio
+                           
+                                                               className='object-cover'
+                                                           />
             </div>
           </div>
         </div>
